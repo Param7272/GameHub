3ee56e24e5ab2c6f25bc12d7f27acb286c1588de
Param7272 <paramjitsingh727282@gmail.com>
2024-02-14 11:21:35 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <h1>Heloo</h1>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+}
+
+export default App;
